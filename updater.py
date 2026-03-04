f96097d0a93205b0b62848d96c38d1856946e4a3
Lars Kellogg-Stedman <lars@oddbit.com>
2026-03-03 22:26:58 -0500

    Make intervals and jitter customizable

--- updater.py
+++ updater.py
@@ -1,9 +1,10 @@
 # pyright: reportUnusedCallResult=false
+
 import argparse
 import logging
 import random
 import time
-from typing import Callable
+from typing import Callable, Protocol
 
 import schedule
 
@@ -22,6 +23,10 @@ sources = {
 }
 
 
+class Driver(Protocol):
+    def update(self): ...
+
+
 def ratelimit():
     delay = random.randint(10, 180)
     LOG.info("waiting for %d seconds before next fetch", delay)
@@ -36,7 +41,7 @@ def wrap_update(source: str, store: storage.Storage, f: Callable[[], None]):
         except storage.IntegrityError as err:
             LOG.warning("caught error from %s: %s", source, err)
 
-        LOG.info("found %d new items", store.getNewItemCount())
+        LOG.info("found %d new items from %s", store.getNewItemCount(), source)
 
     return func
 
@@ -48,40 +53,70 @@ def parse_args() -> argparse.Namespace:
         "--interval",
         "-i",
         action="append",
+        default=[],
         type=lambda s: ((parts := s.split("="))[0], int(parts[1])),
+    )
+
+    p.add_argument(
+        "--jitter",
+        "-j",
+        type=float,
+        default=0.5,
+    )
+
+    p.add_argument(
+        "--source",
+        "-s",
+        action="append",
+    )
+
+    p.add_argument(
+        "--immediately",
+        "-I",
+        action="store_true",
     )
 
     return p.parse_args()
 
 
+def schedule_with_jitter(func, interval: int, jitter: float = 0.5):
+    low = int(interval - (interval * jitter))
+    high = int(interval + (interval * jitter))
+
+    if low <= 0:
+        raise ValueError(f"interval {interval} with jitter {jitter} must be > 0")
+    schedule.every(low).to(high).minutes.do(func)
+
+
 def main():
     args = parse_args()
-    intervals = dict(args.interval)
+    intervals: dict[str, int] = dict(args.interval)
     logging.basicConfig(level="INFO")
     store = storage.SqliteStorage("items.db")
 
-    d_qth = qth.QTH(store, ratelimit=ratelimit)
-    d_mtc = mtc.MTC(store, ratelimit=ratelimit)
-    d_rle = rle.RLE(store, ratelimit=ratelimit)
-    d_qrz = qrz.QRZ(store, ratelimit=ratelimit)
+    drivers: dict[str, tuple[Driver, int]] = {
+        "qth": (qth.QTH(store, ratelimit=ratelimit), 60),
+        "mtc": (mtc.MTC(store, ratelimit=ratelimit), 720),
+        "rle": (rle.RLE(store, ratelimit=ratelimit), 720),
+        "qrz": (qrz.QRZ(store, ratelimit=ratelimit), 60),
+    }
 
-    schedule.every(60).to(120).minutes.do(
-        wrap_update("qth", store, d_qth.update),
-    )
-    schedule.every(60).to(120).minutes.do(
-        wrap_update("qrz", store, d_qrz.update),
-    )
-    schedule.every(interval=720).to(780).minutes.do(
-        wrap_update("mtc", store, d_mtc.update),
-    )
-    schedule.every(interval=720).to(780).minutes.do(
-        wrap_update("rle", store, d_rle.update),
-    )
+    for name, (driver, default_interval) in drivers.items():
+        if args.source is None or name in args.source:
+            schedule_with_jitter(
+                wrap_update(name, store, driver.update),
+                intervals.get(name, default_interval),
+                jitter=args.jitter,
+            )
+
+    if args.immediately:
+        for name, (driver, default_interval) in drivers.items():
+            wrap_update(name, store, driver.update)()
 
     loopcount = 0
     while True:
         idle = schedule.idle_seconds()
-        if idle is not None and loopcount % 120 == 0:
+        if idle is not None and loopcount % 300 == 0:
             LOG.info("%d seconds until next job runs", idle)
         schedule.run_pending()
         loopcount += 1
